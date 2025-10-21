23e0b26ee3bfd85924b0de370afb6c6dbf26a4ba
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-22 02:44:28 +0400

    задание 1,2,3

--- homework/views.py
+++ homework/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
     user_name = 'Руслан'
-    context = {'name':user_name}
+    name = 'Павел'
+    age = 45
+    phone = '+79234567891'
+    email = 'pavel@pavel.com'
+    profile_data={'name':'Павел', 'age':45, 'phone':'+79234567891', 'email':'pavel@pavel.com'} 
+    context = {'user_name':user_name, 'name':name, 'age':age, 'phone':phone, 'email':email, 'profile_data':profile_data}
+    
     return render(request, 'homework/index.html', context)
+
+
+
 
