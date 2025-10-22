f25ec9e5b6c04c5496dae771a136bf16670d5a98
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-22 18:43:25 +0400

    задание 1-7

--- homework/views.py
+++ homework/views.py
@@ -8,9 +8,25 @@ def index(request):
     email = 'pavel@pavel.com'
     profile_data={'name':'Павел', 'age':45, 'phone':'+79234567891', 'email':'pavel@pavel.com'}
     prof_sps=['Павел', 45, '+79234567891', 'pavel@pavel.com'] 
-    context = {'user_name':user_name, 'name':name, 'age':age, 'phone':phone, 'email':email, 'profile_data':profile_data, 'prof_sps':prof_sps}
+    name_sps = ['Павел', 'Алексей']
+    experience = [3, 6]
+    user_list = ['Игорь', 'Андрей', 'Дмитрий', 'Алексей', 'Николай', 'Евгений', 'Иван', 'Павел']
+
+
+    context = {
+        'user_name':user_name, 
+        'name':name, 
+        'age':age, 
+        'phone':phone, 'email':email, 
+        'profile_data':profile_data, 
+        'prof_sps':prof_sps,
+        'name_sps':name_sps,
+        'experience':experience,
+        'user_list':user_list
+        }
 
     return render(request, 'homework/index.html', context)
+
 
 
 
