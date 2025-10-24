25c80be8df6efa7754657ef251a4221499cee21e
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-24 19:36:54 +0400

    модель Article

---
+++ homework_article/article/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Article(models.Model):
+    title   = models.CharField(max_length=200, verbose_name='заголовок')
+    date    = models.DateTimeField(verbose_name='дата')
+    content = models.TextField(verbose_name='содержание')
+    status  = models.IntegerField(verbose_name='статус')
+
