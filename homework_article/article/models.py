24a9780c33336b63070e792dc4b192673900e96e
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-25 00:33:05 +0400

    модель group

--- homework_article/article/models.py
+++ homework_article/article/models.py
@@ -10,3 +10,11 @@ class Article(models.Model):
         verbose_name = 'статьи'
         verbose_name_plural = 'статьи' 
     
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='URL имя')
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:                
+        verbose_name = 'группы'
+        verbose_name_plural = 'группы' 
