a34981375786b23bfb31935ff64aa48262957013
ev-g-hash <pihtulovevgeny@gmail.com>
2025-10-28 07:06:23 +0400

    blog admin.py

--- blog/blog/admin.py
+++ blog/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Category, Comment, Post
+ 
+class CategoryAdmin(admin.ModelAdmin):
+     pass
+ 
+class PostAdmin(admin.ModelAdmin):
+    pass
 
-# Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
