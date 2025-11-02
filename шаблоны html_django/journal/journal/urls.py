33f95f0b6596d89b46a3b10a4b935e30b5a2457a
ev-g-hash <pihtulovevgeny@gmail.com>
2025-11-02 19:57:35 +0400

    journal templates

--- шаблоны html_django/journal/journal/urls.py
+++ шаблоны html_django/journal/journal/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'journal'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics')
 ]
--- шаблоны html_django/journal/journal/views.py
+++ шаблоны html_django/journal/journal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Topic
 
-# Create your views here.
+def index(request):
+    return render(request, 'journal/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+
+    return render(request, 'journal/topics.html', context)
