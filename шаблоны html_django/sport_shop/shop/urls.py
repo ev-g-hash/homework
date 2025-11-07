6d6fa6f5a59f7039b1cc1f7ed42e41a1302ef7f9
ev-g-hash <pihtulovevgeny@gmail.com>
2025-11-07 21:25:10 +0400

    shop - start static templates гтовые шаблоны

--- шаблоны html_django/sport_shop/shop/urls.py
+++ шаблоны html_django/sport_shop/shop/urls.py
@@ -4,4 +4,7 @@ from shop import views
 
 urlpatterns = [
     path('', views.index, name='index'),       
+    path('feedback/', views.feedback, name='feedback'),       
+    path('aboutas/', views.aboutas, name='aboutas'),       
+    path('readmore/', views.readmore, name='readmore'),       
 ]
--- шаблоны html_django/sport_shop/shop/views.py
+++ шаблоны html_django/sport_shop/shop/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
+
+def feedback(request):
+    return render(request, 'shop/feedback.html')
+
+def aboutas(request):
+    return render(request, 'shop/aboutas.html')
+
+def readmore(request):
+    return render(request, 'shop/readmore.html')
