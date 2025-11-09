17825a922585aa7492a3f6ad06e4cba5d04c846c
ev-g-hash <pihtulovevgeny@gmail.com>
2025-11-09 14:44:02 +0400

    отправка формы html в ТГ-бот

---
+++ шаблоны html_django/sport_shop/shop/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Ваше имя', max_length=30)
+    email = forms.CharField(label='Ваш адрес электронной почты', max_length=100)
+    phone = forms.CharField(label='Ваш телефон', max_length=30)
+    message = forms.CharField(label='Сообщение', max_length=200)
--- шаблоны html_django/sport_shop/shop/views.py
+++ шаблоны html_django/sport_shop/shop/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactForm
 
 def index(request):
     return render(request, 'shop/index.html')
-
-def feedback(request):
-    return render(request, 'shop/feedback.html')
 
 def aboutas(request):
     return render(request, 'shop/aboutas.html')
 
 def readmore(request):
     return render(request, 'shop/readmore.html')
+
+def forms(request):
+    if request.method == 'POST':
+        
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        return HttpResponse(f'''
+            <pre>
+                Вы ввели следующие данные
+                Вы ввели имя: {name} 
+                Вы ввели емайл: {email} 
+                Вы ввели телефон: {phone} 
+                Вы ввели сообщение: {message}
+                Ожидайте в ближайшее время мы с вами свяжемся!
+            </pre>            
+        ''')
+    else:
+        userform = ContactForm()
+        return render(request, 'shop/forms.html', {'form':userform})
