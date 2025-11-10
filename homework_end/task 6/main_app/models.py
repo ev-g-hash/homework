d5d45d1f7ff4e7592845b6b95c433e825e6bfe75
ev-g-hash <pihtulovevgeny@gmail.com>
2025-11-10 23:47:27 +0400

    domtask6 - start - venv - activate

--- homework_end/task 6/main_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    age = models.IntegerField()
-    phone = models.CharField(max_length=15)
-    email = models.CharField(max_length=30)
